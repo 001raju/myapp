cedc3abea40a5b41e060af1e0ceb152ab29d5e74
rajumaharjan <raju_maharjan001@hotmail.com>
2022-12-14 19:39:25 -0500

    trying to creat modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import LoginBtn from './Components/LoginBtn';
 import Header from './Header';
+
 
 function App() {
   return (
     <div >
       <div>hello added</div>
       <Header/>
+      <LoginBtn/>
     </div>
   );
 }
---
+++ src/Components/LoginBtn.jsx
@@ -0,0 +1,27 @@
+// import {useState} from "react"
+// import "./LoginBtn.css";
+
+ function LoginBtn(){
+    // const [isOpen, setIsOpen] = useState(false);
+    // function displayLogin(){
+    //   LoginBox.classList.add
+    //}
+    return (
+      <>
+        <div class = "btn">
+            <button onClick ="displayLogin">Log In</button>
+        </div>
+        <div class="loginBox">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <input type="password" />
+          </div>
+        </div>
+      </>
+    );
+}
+export default LoginBtn;
